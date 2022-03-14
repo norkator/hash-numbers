848e115e6a36201a2ad4c9a4ae04436fde7b37db
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2022-03-14 23:05:37 +0200

    add simple test for CRC32

---
+++ src/hash-numbers.test.ts
@@ -0,0 +1,32 @@
+import * as chai from 'chai';
+import {HashNumbers, HashParamsInterface} from './hash-numbers';
+
+const params: HashParamsInterface = {
+    algorithm: 'CRC32',
+    salt: 'Test123',
+    prefix: '',
+    suffix: ''
+}
+
+let arr: number[] = [];
+
+const expect = chai.expect;
+describe('hash-numbers', async () => {
+
+    it('should generate CRC32 ids', async () => {
+        const hashNumber = new HashNumbers(params);
+        for (let i = 1; i < 10000; i++) {
+            const encoded = hashNumber.encode(i);
+            arr.push(Number(encoded));
+            // const decoded = hashNumber.decode(encoded);
+            console.log(encoded);
+        }
+        expect(arr[arr.length - 1]).to.equal(589633172);
+    });
+
+    it('should not have duplicates', async () => {
+        const toFindDuplicates = (arry: number[]) => arry.filter((item: number, index: number) => arr.indexOf(item) !== index)
+        const duplicates = toFindDuplicates(arr);
+        expect(duplicates.length).to.equal(0);
+    });
+});
