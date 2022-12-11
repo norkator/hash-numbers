541700b0bbfdcf00a1c8fe030b0c32ef6269b386
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2022-12-11 20:18:44 +0200

    add sample script and link it to nodemon config

---
+++ src/sample.ts
@@ -0,0 +1,18 @@
+import Index, {HashParamsInterface} from '.';
+
+const params: HashParamsInterface = {
+    algorithm: 'DEFAULT',
+    salt: 'Czb8ZQja',
+    prefix: '',
+    suffix: ''
+}
+const hashNumber = new Index(params);
+
+let arr: number[] = []
+
+for (let i = 1; i < 10; i++) {
+    const encoded = hashNumber.encode(i);
+    arr.push(Number(encoded));
+    const decoded = hashNumber.decode(encoded);
+    console.log(encoded + ' / ' + decoded);
+}
